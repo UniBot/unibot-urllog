47e0bccc21853c531bb3d86bf53ed10fd3ceea61
tarlepp <tarmo.leppanen@protacon.fi>
2014-12-25 19:58:32 +0200

    Refactored plugin to work with rewrite branch.

--- README.md
+++ README.md
@@ -3,8 +3,46 @@
 Simple UniBot plugin to log all URLs from configured channel.
 
 ## Install
-To your UniBot 
+To your UniBot application
 ```npm install git://github.com/UniBot/unibot-urllog --save```
 
-And after that register new plugin on IRC
-```plugin [#channel] urllog
+And after that register new plugin on IRC channels what you need
+```plugin [#channel] urllog```
+
+ps. remember to restart your UnitBot.
+
+## Configuration
+You can configure this plugin adding ```urllog``` section to your UniBot ```config.js``` file. Example below
+
+```
+module.exports = { 
+    ...
+    plugins: {
+        "urllog": {
+            "dateFormat": "D.M.YYYY HH:mm:ss",
+            "oldMessage": "${nick}: Old link!!1! ${existUrl.from} told this already ${formattedDate}"
+        }
+    }
+};
+```
+
+###dateFormat###
+This is a string that ```moment.js``` library ```format()``` function will take. This is used on ```formattedDate``` 
+option on ```oldMessage``` string. Default value for this is ```D.M.YYYY HH:mm:ss``` 
+
+###oldMessage###
+A string that is said on channel if URL on message is an old one (someone else has said this before). Default value for
+this is ```${nick}: Old link!!1! ${existUrl.from} told this already ${formattedDate}``` Within this message you can use 
+following parameters:
+
+```
+url
+nick
+title
+formattedDate
+existUrl.url
+existUrl.description
+existUrl.title
+existUrl.from
+existUrl.timestamp
+```
--- index.js
+++ index.js
@@ -1,78 +1,160 @@
-var mongoose = require('mongoose');
+/**
+ * Plugin dependencies.
+ *
+ * @type {exports}
+ */
 var metaInspector = require('node-metainspector');
 var moment = require('moment');
+var _ = require('lodash');
 
-var Urls = new mongoose.Schema({
-    channel: {
-        type: String, // channel._id
-        index: {
-            unique: true,
-            dropDups: false
-        }
-    },
-    urls: {
-        type: mongoose.Schema.Types.Mixed,
-        default: []
-    }
-});
+/**
+ * URL log plugin for UniBot
+ *
+ * @param  {Object} options Plugin options object, description below.
+ *   db: {mongoose} the mongodb connection
+ *   bot: {irc} the irc bot
+ *   web: {connect} a connect + connect-rest webserver
+ *   config: {object} UniBot configuration
+ *
+ * @return  {Function}  Init function to access shared resources
+ */
+module.exports = function init(options) {
+    var mongoose = options.db;
+    var bot = options.bot;
+    var webserver = options.web;
+    var config = options.config;
 
-var model = mongoose.model('Urls', Urls);
-
-var plugin = function(channel, config) {
-    var urls = [];
-
-    model.findOne({channel: channel.id}, function(error, _urls) {
-        if (error || !_urls) {
-            urls = new model({
-                channel: channel.id
-            });
-
-            urls.save();
-        } else {
-            urls = _urls;
+    // Specify database schema for plugin
+    var Urls = new mongoose.Schema({
+        channel: {
+            type: String, // channel._id
+            index: {
+                unique: true,
+                dropDups: false
+            }
+        },
+        urls: {
+            type: mongoose.Schema.Types.Mixed,
+            default: []
         }
     });
 
-    return {
-        "^((?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))(?::\\d{2,5})?(?:/\\S*)?)(\\s+(.*))?": function(from, matches) {
-            var url = matches[1];
-            var description = matches[3];
-            var client = new metaInspector(url, {});
+    var model = mongoose.model('Urls', Urls);
 
-            model.findOne({channel: channel.id, 'urls.url': url}, function(error, _url) {
-                if (error) {
-                    channel.say('Oh noes, error: ' + error, from);
-                } else if (_url) {
-                    var date = moment(_url.urls[0].timestamp);
-                    var message = from + ": Old link!!1! " + _url.urls[0].from + " told this already " + date.format('D.M.YYYY HH:mm:ss');
+    /**
+     * Default plugin configuration. These can be override on your UniBot config.js file
+     *
+     * @type    {{
+     *              dateFormat: string,
+     *              oldMessage: string
+     *          }}
+     */
+    var pluginConfig = {
+        "dateFormat": "D.M.YYYY HH:mm:ss",
+        "oldMessage": "${nick}: Old link!!1! ${existUrl.from} told this already ${formattedDate}"
+    };
 
-                    channel.say(message);
-                } else {
-                    client.on('fetch', function() {
-                        var data = {
+    // Merge configuration for plugin
+    if (!_.isUndefined(config.plugins.urllog) && _.isObject(config.plugins.urllog)) {
+        pluginConfig = _.merge(pluginConfig, config.plugins.urllog);
+    }
+
+    /**
+     * Getter method for plugin template for UniBot frontend.
+     *
+     * @param   {Request}   req     Request object
+     * @param   {Response}  res     Response object
+     * @param   {Function}  next    Callback function
+     */
+    webserver.get('/urllog', function(req, res, next) {
+        res.sendFile(__dirname + '/index.html');
+    });
+
+    /**
+     * Getter method for URL log data for UniBot frontend.
+     *
+     * @param   {Request}   req     Request object
+     * @param   {Response}  res     Response object
+     * @param   {Function}  next    Callback function
+     */
+    webserver.get('/urllog/:channel', function(req, res, next) {
+        model.findOne({ channel: req.params.channel }, function(err, logs) {
+            res.send(err || logs);
+        });
+    });
+
+    /**
+     * Actual UniBot URL log plugin. This will match all URLs from IRC channel and stores those to database for later
+     * usage. Plugin specified regex will match "any real url" on channel messages.
+     *
+     * If URL has been already stored to database plugin will inform that user about that.
+     */
+    return function plugin(channel){
+        var urls = [];
+
+        // Initial of URL log store
+        model.findOne({ channel: channel.id }, function(err, _urls_) {
+            if (err || !_urls_) {
+                urls = new model({
+                    channel: channel.id
+                });
+
+                urls.save();
+            } else {
+                urls = _urls_;
+            }
+        });
+
+        return {
+            "[.*]?((?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))(?::\\d{2,5})?(?:/\\S*)?)[\\s+]?(.*)?": function(from, matches) {
+                var url = matches[1];
+                var description = matches[2];
+                var client = new metaInspector(url, {});
+
+                model.findOne({channel: channel.id, 'urls.url': url}, function(error, _url) {
+                    if (error) {
+                        channel.say('Oh noes, error: ' + error, from);
+                    } else if (_url) {
+                        // Specify used template variables
+                        var templateVars = {
                             url: url,
-                            description: description,
+                            nick: from,
                             title: client.title,
-                            from: from,
-                            timestamp: Date.now()
+                            formattedDate: moment(_url.urls[0].timestamp).format(pluginConfig.dateFormat),
+                            existUrl: _url.urls[0]
                         };
 
-                        urls.urls.push(data);
-                        urls.markModified('urls');
-                        urls.save();
+                        if (_url.from !== from) {
+                            channel.say(_.template(pluginConfig.oldMessage, templateVars));
+                        }
+                    } else {
+                        client.on('fetch', function() {
+                            var data = {
+                                url: url,
+                                description: description,
+                                title: client.title,
+                                from: from,
+                                timestamp: Date.now()
+                            };
 
-                        channel.say(client.title);
-                    });
+                            urls.urls.push(data);
+                            urls.markModified('urls');
+                            urls.save();
 
-                    client.on('error', function(error) {
-                        channel.say('Oh noes, error: ' + error, from);
-                    });
+                            // If URL has "real" title say that on channel
+                            if (client.title) {
+                                channel.say(client.title);
+                            }
+                        });
 
-                    client.fetch();
-                }
-            });
-        }
+                        client.on('error', function(error) {
+                            channel.say('Oh noes, error: ' + error, from);
+                        });
+
+                        client.fetch();
+                    }
+                });
+            }
+        };
     };
 };
-
-module.exports = plugin;
